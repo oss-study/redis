63b450feeeda52dcbfc4c843f8329e5779e9b45f
wingsxdu <beihai@wingsxdu.com>
2020-01-03 22:49:24 +0800

    添加跳跃表、整数集合结构体注释

--- src/server.h
+++ src/server.h
@@ -858,18 +858,27 @@ struct sharedObjectsStruct {
 
 /* ZSETs use a specialized version of Skiplists */
 typedef struct zskiplistNode {
+    // 成员对象
     sds ele;
+    // 分值
     double score;
+    // 后退指针
     struct zskiplistNode *backward;
+    // 层
     struct zskiplistLevel {
+        // 前进指针
         struct zskiplistNode *forward;
+        // 跨度
         unsigned long span;
     } level[];
 } zskiplistNode;
 
 typedef struct zskiplist {
+    // 表头节点和表尾节点
     struct zskiplistNode *header, *tail;
+    // 表中节点的数量
     unsigned long length;
+    // 表中层数最大的节点的层数
     int level;
 } zskiplist;
 
